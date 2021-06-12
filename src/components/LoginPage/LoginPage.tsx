a203c600b794e16ab97d64214dc020b39816763e
Hector Jimenez Blanco <hectorj@microsoft.com>
2021-06-11 17:08:06 -0700

    Login page and styling system

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,270 +1,66 @@
-import React, { ChangeEvent, useState, FormEvent } from 'react';
-import { useAppState } from '../../state';
-
-import Button from '@material-ui/core/Button';
-import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
-import Grid from '@material-ui/core/Grid';
-import { ReactComponent as GoogleLogo } from './google-logo.svg';
-import { InputLabel, Theme } from '@material-ui/core';
-import IntroContainer from '../IntroContainer/IntroContainer';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-
-import { makeStyles } from '@material-ui/core/styles';
-import { useLocation, useHistory, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import firebase from 'firebase';
+import React from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import { Url } from 'node:url';
-
-const useStyles = makeStyles((theme: Theme) => ({
-  LoginFormContainer: {
-    width: '50%',
-    backgroundColor: 'red',
-    alignItems: 'center',
-    height: '50%',
-    border: '1',
-    borderWidth: '2px',
-  },
-  errorMessage: {
-    color: 'red',
-    display: 'flex',
-    alignItems: 'center',
-    margin: '1em 0 0.2em',
-    '& svg': {
-      marginRight: '0.4em',
-    },
-  },
-  spacer: {
-    margin: '100px',
-  },
-  gutterBottom: {
-    marginBottom: '1em',
-    textAlign: 'center',
-  },
-  passcodeContainer: {
-    minHeight: '120px',
-  },
-  submitButton: {
-    [theme.breakpoints.down('sm')]: {
-      width: '100%',
-    },
-  },
-}));
-
-export default function LoginPage() {
-  const classes = useStyles();
-  const { signIn, user, isAuthReady } = useAppState();
-  const history = useHistory();
-  const location = useLocation<{ from: Location }>();
-  const [passcode, setPasscode] = useState('');
-  const [authError, setAuthError] = useState<Error | null>(null);
-
-  const isAuthEnabled = Boolean(process.env.REACT_APP_SET_AUTH);
-
-  if (user || !isAuthEnabled) {
-    return <Redirect to="/welcome"></Redirect>;
-  }
-
-  if (!isAuthReady) {
-    return null;
-  }
-
-  const uiConfig = {
-    callbacks: {
-      signInSuccessWithAuthResult: function(authResult: AuthenticatorResponse, redirectUrl: Url) {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        var user = firebase.auth().currentUser;
-        var name, email, photoUrl, uid, emailVerified;
-
-        if (user != null) {
-          name = user.displayName;
-          email = user.email;
-          photoUrl = user.photoURL;
-          emailVerified = user.emailVerified;
-          uid = user.uid; // The user's ID, unique to the Firebase project. Do NOT use
-          // this value to authenticate with your backend server, if
-          // you have one. Use User.getToken() instead.
-
-          //get toom name and display name from db
-          console.log(name);
-        }
-
-        return true;
-      },
-    },
-    // Popup signin flow rather than redirect flow.
-    signInFlow: 'popup',
-    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-    signInSuccessUrl: '/',
-    // We will display Google and Facebook as auth providers.
-    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID, firebase.auth.EmailAuthProvider.PROVIDER_ID],
-  };
-
-  return (
-    <IntroContainer>
-      {process.env.REACT_APP_SET_AUTH === 'firebase' && (
-        <>
-          <Typography variant="h5" className={classes.gutterBottom}>
-            Sign in or create account
-          </Typography>
-          <div className={classes.spacer}></div>
-          <div className={classes.spacer}></div>
-          <div>
-            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-          </div>
-        </>
-      )}
-    </IntroContainer>
-  );
-}
-
-/*
-import React, { ChangeEvent, useState, FormEvent } from 'react';
 import { useAppState } from '../../state';
 
-import Button from '@material-ui/core/Button';
-import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
-import Grid from '@material-ui/core/Grid';
-import { ReactComponent as GoogleLogo } from './google-logo.svg';
-import { InputLabel, Theme } from '@material-ui/core';
-import IntroContainer from '../IntroContainer/IntroContainer';
-import TextField from '@material-ui/core/TextField';
-import Typography from '@material-ui/core/Typography';
-
-import { makeStyles } from '@material-ui/core/styles';
-import { useLocation, useHistory } from 'react-router-dom';
-
-const useStyles = makeStyles((theme: Theme) => ({
-  googleButton: {
-    background: 'white',
-    color: 'rgb(0, 94, 166)',
-    borderRadius: '4px',
-    border: '2px solid rgb(2, 122, 197)',
-    margin: '1.8em 0 0.7em',
-    textTransform: 'none',
-    boxShadow: 'none',
-    padding: '0.3em 1em',
-    [theme.breakpoints.down('sm')]: {
-      width: '100%',
-    },
-    '&:hover': {
-      background: 'white',
-      boxShadow: 'none',
-    },
-  },
-  errorMessage: {
-    color: 'red',
-    display: 'flex',
-    alignItems: 'center',
-    margin: '1em 0 0.2em',
-    '& svg': {
-      marginRight: '0.4em',
-    },
-  },
-  gutterBottom: {
-    marginBottom: '1em',
-  },
-  passcodeContainer: {
-    minHeight: '120px',
-  },
-  submitButton: {
-    [theme.breakpoints.down('sm')]: {
-      width: '100%',
-    },
-  },
-}));
+import './LoginPage.scss';
 
 export default function LoginPage() {
-  const classes = useStyles();
-  const { signIn, user, isAuthReady } = useAppState();
-  const history = useHistory();
-  const location = useLocation<{ from: Location }>();
-  const [passcode, setPasscode] = useState('');
-  const [authError, setAuthError] = useState<Error | null>(null);
+    const { user, isAuthReady } = useAppState();
 
-  const isAuthEnabled = Boolean(process.env.REACT_APP_SET_AUTH);
+    if (user) {
+        return <Redirect to="/welcome"></Redirect>;
+    }
 
-  const login = () => {
-    setAuthError(null);
-    signIn?.(passcode)
-      .then(() => {
-        history.replace(location?.state?.from || { pathname: '/' });
-      })
-      .catch(err => setAuthError(err));
-  };
+    if (!isAuthReady) {
+        return null;
+    }
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    login();
-  };
+    const uiConfig = {
+        callbacks: {
+            signInSuccessWithAuthResult: function() {
+                // User successfully signed in.
+                // Return type determines whether we continue the redirect automatically
+                // or whether we leave that to developer to handle.
+                var user = firebase.auth().currentUser;
+                var name, email, photoUrl, uid, emailVerified;
 
-  if (user || !isAuthEnabled) {
-    history.replace('/');
-  }
+                if (user != null) {
+                    name = user.displayName;
+                    email = user.email;
+                    photoUrl = user.photoURL;
+                    emailVerified = user.emailVerified;
+                    uid = user.uid; // The user's ID, unique to the Firebase project. Do NOT use
+                    // this value to authenticate with your backend server, if
+                    // you have one. Use User.getToken() instead.
 
-  if (!isAuthReady) {
-    return null;
-  }
+                    //get toom name and display name from db
+                    console.log(name);
+                }
 
-  return (
-    <IntroContainer>
-      {process.env.REACT_APP_SET_AUTH === 'firebase' && (
-        <>
-          <Typography variant="h5" className={classes.gutterBottom}>
-            Sign in to join a room
-          </Typography>
-          <Typography variant="body1">Sign in using your Twilio Google Account</Typography>
-          <Button variant="contained" className={classes.googleButton} onClick={login} startIcon={<GoogleLogo />}>
-            Sign in with Google
-          </Button>
-        </>
-      )}
+                return true;
+            },
+        },
+        // Popup signin flow rather than redirect flow.
+        signInFlow: 'popup',
+        // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+        signInSuccessUrl: '/',
+        // We will display Google and Facebook as auth providers.
+        signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID, firebase.auth.EmailAuthProvider.PROVIDER_ID],
+    };
 
-      {process.env.REACT_APP_SET_AUTH === 'passcode' && (
-        <>
-          <Typography variant="h5" className={classes.gutterBottom}>
-            Enter passcode to join a room
-          </Typography>
-          <form onSubmit={handleSubmit}>
-            <Grid container justify="space-between">
-              <div className={classes.passcodeContainer}>
-                <InputLabel shrink htmlFor="input-passcode">
-                  Passcode
-                </InputLabel>
-                <TextField
-                  id="input-passcode"
-                  onChange={(e: ChangeEvent<HTMLInputElement>) => setPasscode(e.target.value)}
-                  type="password"
-                  variant="outlined"
-                  size="small"
-                />
-                <div>
-                  {authError && (
-                    <Typography variant="caption" className={classes.errorMessage}>
-                      <ErrorOutlineIcon />
-                      {authError.message}
-                    </Typography>
-                  )}
+    return (
+        <div className="login-page">
+            <div className="modal">
+                <div className="modal-brand"></div>
+                <div className="modal-content">
+                    <h5>Sign in or Create account</h5>
+                    <div>
+                        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+                    </div>
                 </div>
-              </div>
-            </Grid>
-            <Grid container justify="flex-end">
-              <Button
-                variant="contained"
-                color="primary"
-                type="submit"
-                disabled={!passcode.length}
-                className={classes.submitButton}
-              >
-                Submit
-              </Button>
-            </Grid>
-          </form>
-        </>
-      )}
-    </IntroContainer>
-  );
+            </div>
+        </div>
+    );
 }
-*/
